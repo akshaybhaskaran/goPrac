b22b0416325174e044fa7a6c3666d9b72367a93a
Akshay Bhaskaran <bhaskaran.a@husky.neu.edu>
2019-03-03 22:19:48 -0600

    linear search | O(n)

--- linearSearch.go
+++ linearSearch.go
@@ -8,13 +8,17 @@ func linearSearch(list[]int, key int) bool{
 			return true
 		}
 	}
-
 	return false
 }
-
 
 func main(){
 	items := []int {22,45,67,54,26,89,54,67,32,76,44}
 	key := 10
 	fmt.Println(linearSearch(items, key))
+
 }
+
+
+
+//Worst cast complexity is O(n). 
+//Time increases as the number of elements increases
